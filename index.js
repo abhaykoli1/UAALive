28738a2c7d0f1651db368d0395ddcc0615264ca5
Abhay <abhaykoli214@gmail.com>
2025-02-14 22:11:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Serve index.html when visiting "/"
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start the server
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
