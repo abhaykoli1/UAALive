348356ce11a735ccf1827c20d1b51c71ec7ddbd3
Abhay <abhaykoli214@gmail.com>
2025-02-18 21:36:50 +0530

    changes

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // Start the server
 const PORT = 8080;
